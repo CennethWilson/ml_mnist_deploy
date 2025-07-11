7662e9accff66502ec8b468447a97d24b2dd4e15
CennethWilson <116559838+CennethWilson@users.noreply.github.com>
2025-07-11 14:08:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -79,5 +79,5 @@ def ping():
     return "pong", 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
