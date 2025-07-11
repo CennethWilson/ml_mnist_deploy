831fb5716950174dfe260b6db51140dde368f46a
CennethWilson <116559838+CennethWilson@users.noreply.github.com>
2025-07-11 13:23:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -77,4 +77,6 @@ def predict():
 def ping():
     return "pong", 200
 
-app.run(port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
